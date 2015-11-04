56d4d6f0ec04db675c4c68342b963a9cbd6c9b15
nadim <i.nadim@gmail.com>
2015-11-03 22:01:56 -0500

    settings update for heroku

--- main_wearhacks/settings/prod.py
+++ main_wearhacks/settings/prod.py
@@ -2,8 +2,11 @@ DEBUG = TEMPLATE_DEBUG = False  # production
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 import os
-DATABASES['default'] =  dj_database_url.config()
 
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
