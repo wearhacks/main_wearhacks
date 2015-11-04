dfd4b0ecfe7564cc80899c770cccf1e52cff1382
nadim <i.nadim@gmail.com>
2015-11-03 22:38:35 -0500

    prod settings

--- main_wearhacks/settings/prod.py
+++ main_wearhacks/settings/prod.py
@@ -2,7 +2,7 @@ DEBUG = TEMPLATE_DEBUG = False  # production
 # Parse database configuration from $DATABASE_URL
 import dj_database_url
 import os
-from .common import SITE_NAME, DJANGO_ROOT
+from .common import SITE_NAME, DJANGO_ROOT, SITE_ROOT
 
 DATABASES = {
     'default': dj_database_url.config()
@@ -22,9 +22,8 @@ MAILCHIMP_LIST_ID = os.environ.get('MAILCHIMP_LIST_ID', '')
 
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
-
+print DJANGO_ROOT
 STATICFILES_DIRS = (
-    os.path.join(DJANGO_ROOT, 'static')
+    os.path.join(SITE_ROOT, 'static'),
 )
-STATICFILES_FINDERS = (
-)
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
