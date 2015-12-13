fe7acf45aa110e3280fe22a874c0afdbf275a0ab
nadim <i.nadim@gmail.com>
2015-12-13 17:23:56 -0500

    some freebies <3

--- events/views.py
+++ events/views.py
@@ -30,7 +30,8 @@ def home(request):
         'blog_link' : posts[0]["url"],
         'blog_image' : posts[0]["thumbnail_images"]["full"]["url"],
         'event' : event,
-        'slides' : Slider.objects.filter(slider_location = 0).order_by('order')
+        'slides' : Slider.objects.filter(slider_location = 0).order_by('order'),
+        'past_events': Event.objects.all().filter(start_date__lt = datetime.datetime.now()).order_by('start_date')[:5],
     }
 
 
