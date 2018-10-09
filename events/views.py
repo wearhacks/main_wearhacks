01f3745c9d786d6f12429b60473355f8d7aa44ed
Nadim Islam <i.nadim@gmail.com>
2018-10-09 16:50:34 -0400

    Update views.py
    remove blog

--- events/views.py
+++ events/views.py
@@ -25,7 +25,6 @@ def home(request):
         projects = Project.objects.filter(project_type='2').order_by('?')[:num_projects]
         return projects
 
-    posts = json.loads(urlopen(config.A_BLOG_LINK + '/?json=1').read())["posts"]
     events = Event.objects.all().filter(start_date__gt = datetime.datetime.now()).order_by('start_date')
     if len(events) > 0:
         event = events[0]
@@ -34,10 +33,10 @@ def home(request):
     content = {
         'title' : "Home",
         'config':config,
-        'blog_title' : posts[0]["title"],
-        'blog_excerpt' : posts[0]["excerpt"],
-        'blog_link' : posts[0]["url"],
-        'blog_image' : posts[0]["thumbnail_images"]["full"]["url"],
+        'blog_title' : '',
+        'blog_excerpt' : '',
+        'blog_link' : '',
+        'blog_image' : '',
         'event' : event,
         'slides' : Slider.objects.filter(slider_location = 0).order_by('order'),
         'past_events': Event.objects.filter(pastevent__in = PastEvent.objects.all())
